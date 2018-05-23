48ae76e35423483dbc2e0849a0e1531f9bbef08e
Gregory Lemercier <g.lemercier@samsung.com>
2018-05-23 16:11:05 +0200

    connectivity/network: improve reliability when reaching remote servers
    This patch improves reliability of the following functions:
    
    get_current_public_ip: use a different remote API for fetching
    public IP. The one that was previously used ('checkip.org') had
    its response time greatly vary from one call to another.
    
    get_online_status: rework retry mechanism to include resending
    the ICMP frame in case it was lost. Timeout parameter is now
    considered globally, which means we can have several retries
    within the same period until we reach the timeout.
    
    Change-Id: I0ee0f3d7e5c633479de5186c25a5f36b6e797b70
    Signed-off-by: Gregory Lemercier <g.lemercier@samsung.com>

--- src/modules/connectivity/network/artik_network.c
+++ src/modules/connectivity/network/artik_network.c
@@ -19,19 +19,38 @@
 #include <string.h>
 #include <stdio.h>
 #include <errno.h>
+#include <unistd.h>
+#include <time.h>
 
 #include <artik_module.h>
 #include <artik_http.h>
 #include <artik_log.h>
 #include <artik_network.h>
 
-#include <unistd.h>
-
 #include "common_network.h"
 #include "os_network.h"
 
-#define ARTIK_CURRENT_IP_URL	"http://www.checkip.org/"
-#define ARRAY_SIZE(a)		(sizeof(a) / sizeof((a)[0]))
+static const char geoipdb_root_ca[] =
+	"-----BEGIN CERTIFICATE-----\n"
+	"MIIDSjCCAjKgAwIBAgIQRK+wgNajJ7qJMDmGLvhAazANBgkqhkiG9w0BAQUFADA/\n"
+	"MSQwIgYDVQQKExtEaWdpdGFsIFNpZ25hdHVyZSBUcnVzdCBDby4xFzAVBgNVBAMT\n"
+	"DkRTVCBSb290IENBIFgzMB4XDTAwMDkzMDIxMTIxOVoXDTIxMDkzMDE0MDExNVow\n"
+	"PzEkMCIGA1UEChMbRGlnaXRhbCBTaWduYXR1cmUgVHJ1c3QgQ28uMRcwFQYDVQQD\n"
+	"Ew5EU1QgUm9vdCBDQSBYMzCCASIwDQYJKoZIhvcNAQEBBQADggEPADCCAQoCggEB\n"
+	"AN+v6ZdQCINXtMxiZfaQguzH0yxrMMpb7NnDfcdAwRgUi+DoM3ZJKuM/IUmTrE4O\n"
+	"rz5Iy2Xu/NMhD2XSKtkyj4zl93ewEnu1lcCJo6m67XMuegwGMoOifooUMM0RoOEq\n"
+	"OLl5CjH9UL2AZd+3UWODyOKIYepLYYHsUmu5ouJLGiifSKOeDNoJjj4XLh7dIN9b\n"
+	"xiqKqy69cK3FCxolkHRyxXtqqzTWMIn/5WgTe1QLyNau7Fqckh49ZLOMxt+/yUFw\n"
+	"7BZy1SbsOFU5Q9D8/RhcQPGX69Wam40dutolucbY38EVAjqr2m7xPi71XAicPNaD\n"
+	"aeQQmxkqtilX4+U9m5/wAl0CAwEAAaNCMEAwDwYDVR0TAQH/BAUwAwEB/zAOBgNV\n"
+	"HQ8BAf8EBAMCAQYwHQYDVR0OBBYEFMSnsaR7LHH62+FLkHX/xBVghYkQMA0GCSqG\n"
+	"SIb3DQEBBQUAA4IBAQCjGiybFwBcqR7uKGY3Or+Dxz9LwwmglSBd49lZRNI+DT69\n"
+	"ikugdB/OEIKcdBodfpga3csTS7MgROSR6cz8faXbauX+5v3gTt23ADq1cEmv8uXr\n"
+	"AvHRAosZy5Q6XkjEGB5YGV8eAlrwDPGxrancWYaLbumR9YbK+rlmM6pZW87ipxZz\n"
+	"R8srzJmwN0jP41ZL9c8PDHIyh8bwRLtTcm1D9SZImlJnt1ir/md2cXjbDaJWFBM5\n"
+	"JDGFoqgCWjBH4d1QB7wCCZAA62RjYJsWvIjJEubSfZGL+T0yjWW06XyxV3bqxbYo\n"
+	"Ob8VZRzI9neWagqNdwvYkQsEjgfbKbYK7p2CNTUQ\n"
+	"-----END CERTIFICATE-----\n";
 
 static artik_error artik_set_network_config(
 		artik_network_config * config,
@@ -83,25 +102,23 @@ artik_error artik_get_current_public_ip(artik_network_ip *ip)
 	char *response = NULL;
 	char *point;
 	char *token = NULL;
-	char delimiter[] = "<>";
+	char delimiter[] = "\"";
 	unsigned int size = 0;
-	artik_http_headers headers;
-	artik_http_header_field fields[] = {
-		{"user-agent", "Artik browser"},
-		{"Accept-Language", "en-US,en;q=0.8"},
-	};
+	artik_ssl_config ssl;
 
 	log_dbg("");
 
-	headers.fields = fields;
-	headers.num_fields = ARRAY_SIZE(fields);
+	memset(&ssl, 0, sizeof(artik_ssl_config));
+	ssl.verify_cert = ARTIK_SSL_VERIFY_REQUIRED;
+	ssl.ca_cert.data = (char *)geoipdb_root_ca;
+	ssl.ca_cert.len = sizeof(geoipdb_root_ca);
 
 	/* Perform the request */
-	ret = http->get(ARTIK_CURRENT_IP_URL, &headers, &response, NULL, NULL);
+	ret = http->get("https://geoip-db.com/json/", NULL, &response, NULL, &ssl);
 	if (ret != S_OK)
 		goto exit;
 
-	point = strstr(response, "Your IP Address:");
+	point = strstr(response, "\"IPv4\":\"");
 	if (point != NULL) {
 		token = strtok(point, delimiter);
 		for (i = 0; token != NULL && i < 2; i++) {
@@ -130,11 +147,18 @@ artik_error artik_get_online_status(const char *addr, int timeout, bool *online_
 	struct sockaddr_storage to;
 	ssize_t len;
 	socklen_t fromlen = sizeof(from);
+	struct timespec ts;
+	uint64_t start_ms, curr_ms;
 
-	if (!online_status || !addr || !(timeout > 0))
+	if (!online_status || !addr)
 		return E_BAD_ARGS;
 
 	log_dbg("");
+
+	if (timeout > 0) {
+		clock_gettime(CLOCK_MONOTONIC, &ts);
+		start_ms = (ts.tv_sec * 1000) + (ts.tv_nsec / 1000000);
+	}
 
 	*online_status = false;
 
@@ -145,23 +169,35 @@ artik_error artik_get_online_status(const char *addr, int timeout, bool *online_
 		return E_NETWORK_ERROR;
 	}
 
-	sock = create_icmp_socket(timeout);
+	sock = create_icmp_socket(timeout / 5);
 	if (sock < 0) {
 		log_err("Failed to create ICMP socket");
 		return E_NETWORK_ERROR;
 	}
 
+try_again:
 	if (!os_send_echo(sock, (struct sockaddr *)&to, 0)) {
 		log_err("Failed to send ICMP frame");
 		close(sock);
 		return E_NETWORK_ERROR;
 	}
 
-try_again:
 	len = recvfrom(sock, buf, sizeof(buf), 0, (struct sockaddr *)&from, &fromlen);
 	if (len <= 0) {
-		if (errno == EAGAIN)
+		if (errno == EAGAIN) {
+			if (timeout > 0) {
+				clock_gettime(CLOCK_MONOTONIC, &ts);
+				curr_ms = (ts.tv_sec * 1000) + (ts.tv_nsec / 1000000);
+				if ((curr_ms - start_ms) > timeout) {
+					log_dbg("Timed out waiting for the ping reply");
+					close(sock);
+					return E_TIMEOUT;
+				}
+			}
+
+			sleep(1);
 			goto try_again;
+		}
 
 		log_err("recvfrom: unable to receive data (err=%d)", errno);
 		close(sock);
