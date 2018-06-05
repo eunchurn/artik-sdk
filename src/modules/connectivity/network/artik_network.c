fccfb9f65ded2995dd65498a5b5d0924a1e84052
Gregory Lemercier <g.lemercier@samsung.com>
2018-06-05 11:41:32 +0200

    network: change API to fetch public IP
    This patch changes the external APIs used to retrieve the
    board's public IP address. The selected API is more reliable
    and accessible from China.
    
    Change-Id: I2013b1ba44016de248113c4fd36f468036480db9
    Signed-off-by: Gregory Lemercier <g.lemercier@samsung.com>

--- src/modules/connectivity/network/artik_network.c
+++ src/modules/connectivity/network/artik_network.c
@@ -30,8 +30,27 @@
 
 #include "os_network.h"
 
-#define ARTIK_CURRENT_IP_URL	"http://www.checkip.org/"
-#define ARRAY_SIZE(a)		(sizeof(a) / sizeof((a)[0]))
+static const char geoipdb_root_ca[] =
+	"-----BEGIN CERTIFICATE-----\n"
+	"MIIDSjCCAjKgAwIBAgIQRK+wgNajJ7qJMDmGLvhAazANBgkqhkiG9w0BAQUFADA/\n"
+	"MSQwIgYDVQQKExtEaWdpdGFsIFNpZ25hdHVyZSBUcnVzdCBDby4xFzAVBgNVBAMT\n"
+	"DkRTVCBSb290IENBIFgzMB4XDTAwMDkzMDIxMTIxOVoXDTIxMDkzMDE0MDExNVow\n"
+	"PzEkMCIGA1UEChMbRGlnaXRhbCBTaWduYXR1cmUgVHJ1c3QgQ28uMRcwFQYDVQQD\n"
+	"Ew5EU1QgUm9vdCBDQSBYMzCCASIwDQYJKoZIhvcNAQEBBQADggEPADCCAQoCggEB\n"
+	"AN+v6ZdQCINXtMxiZfaQguzH0yxrMMpb7NnDfcdAwRgUi+DoM3ZJKuM/IUmTrE4O\n"
+	"rz5Iy2Xu/NMhD2XSKtkyj4zl93ewEnu1lcCJo6m67XMuegwGMoOifooUMM0RoOEq\n"
+	"OLl5CjH9UL2AZd+3UWODyOKIYepLYYHsUmu5ouJLGiifSKOeDNoJjj4XLh7dIN9b\n"
+	"xiqKqy69cK3FCxolkHRyxXtqqzTWMIn/5WgTe1QLyNau7Fqckh49ZLOMxt+/yUFw\n"
+	"7BZy1SbsOFU5Q9D8/RhcQPGX69Wam40dutolucbY38EVAjqr2m7xPi71XAicPNaD\n"
+	"aeQQmxkqtilX4+U9m5/wAl0CAwEAAaNCMEAwDwYDVR0TAQH/BAUwAwEB/zAOBgNV\n"
+	"HQ8BAf8EBAMCAQYwHQYDVR0OBBYEFMSnsaR7LHH62+FLkHX/xBVghYkQMA0GCSqG\n"
+	"SIb3DQEBBQUAA4IBAQCjGiybFwBcqR7uKGY3Or+Dxz9LwwmglSBd49lZRNI+DT69\n"
+	"ikugdB/OEIKcdBodfpga3csTS7MgROSR6cz8faXbauX+5v3gTt23ADq1cEmv8uXr\n"
+	"AvHRAosZy5Q6XkjEGB5YGV8eAlrwDPGxrancWYaLbumR9YbK+rlmM6pZW87ipxZz\n"
+	"R8srzJmwN0jP41ZL9c8PDHIyh8bwRLtTcm1D9SZImlJnt1ir/md2cXjbDaJWFBM5\n"
+	"JDGFoqgCWjBH4d1QB7wCCZAA62RjYJsWvIjJEubSfZGL+T0yjWW06XyxV3bqxbYo\n"
+	"Ob8VZRzI9neWagqNdwvYkQsEjgfbKbYK7p2CNTUQ\n"
+	"-----END CERTIFICATE-----\n";
 
 static artik_error artik_set_network_config(
 		artik_network_config * config,
@@ -78,25 +97,23 @@ artik_error artik_get_current_public_ip(artik_network_ip *ip)
 	char *response = NULL;
 	char *point;
 	char *token = NULL;
-	char delimiter[] = "<>";
+	char delimiter[] = "\"";
 	unsigned int size = 0;
-	artik_http_headers headers;
-	artik_http_header_field fields[] = {
-		{"user-agent", "Artik browser"},
-		{"Accept-Language", "en-US,en;q=0.8"},
-	};
+	artik_ssl_config ssl;
 
 	log_dbg("");
 
-	headers.fields = fields;
-	headers.num_fields = ARRAY_SIZE(fields);
+	memset(&ssl, 0, sizeof(artik_ssl_config));
+	ssl.verify_cert = ARTIK_SSL_VERIFY_REQUIRED;
+	ssl.ca_cert.data = (char *)geoipdb_root_ca;
+	ssl.ca_cert.len = sizeof(geoipdb_root_ca);
 
 	/* Perform the request */
-	ret = http->get(ARTIK_CURRENT_IP_URL, &headers, &response, NULL, false);
+	ret = http->get("http://geoip.nekudo.com/api", NULL, &response, NULL, &ssl);
 	if (ret != S_OK)
 		goto exit;
 
-	point = strstr(response, "Your IP Address:");
+	point = strstr(response, "\"ip\":");
 	if (point != NULL) {
 		token = strtok(point, delimiter);
 		for (i = 0; token != NULL && i < 2; i++) {
