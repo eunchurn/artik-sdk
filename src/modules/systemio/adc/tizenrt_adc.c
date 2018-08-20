6adeddb1aa95bb73eb04f7a946482e8dc7a35d94
Gregory Lemercier <g.lemercier@samsung.com>
2018-08-20 15:36:34 +0200

    tizenrt/adc: fix issue when doing multiple reads in a row
    Previously the open call to the ADC driver was done in the
    'request' function and the fd was closed after reading in the
    'read' function, which caused subsequent reads to fail since
    the fd was closed.
    
    This patch fixes the issue by doing open/close on the driver
    once in the 'request' function to check the pin number parameter,
    then calls open/close again for each read of the ADC value. It
    seems the latter is needed to properly reinitialize the ADC
    conversion between two reads, otherwise the conversion sometimes
    returns irrelevant values on the second read.
    
    Change-Id: I2a63436e53d53c2928ceca42043fe5d65ad71592
    Signed-off-by: Gregory Lemercier <g.lemercier@samsung.com>

--- src/modules/systemio/adc/tizenrt_adc.c
+++ src/modules/systemio/adc/tizenrt_adc.c
@@ -31,84 +31,78 @@
 #include <tinyara/analog/adc.h>
 #include <tinyara/analog/ioctl.h>
 
-typedef struct {
-	int fd;
-	char *path;
-} artik_adc_user_data_t;
-
 #define MAX_SIZE 128
 #define S5J_ADC_MAX_CHANNELS	4
 
 artik_error os_adc_request(artik_adc_config *config)
 {
-	artik_adc_user_data_t *user_data = NULL;
+	char *path = NULL;
+	int fd = 0;
 
 	if (!config)
 		return E_BAD_ARGS;
 
-	user_data = malloc(sizeof(artik_adc_user_data_t));
-	if (!user_data)
+	path = malloc(MAX_SIZE + 1);
+	if (!path)
 		return E_NO_MEM;
 
-	user_data->path = malloc(MAX_SIZE + 1);
-	if (!user_data->path) {
-		free(user_data);
-		return E_NO_MEM;
-	}
+	snprintf(path, MAX_SIZE, "/dev/adc%d", config->pin_num);
 
-	snprintf(user_data->path, MAX_SIZE, "/dev/adc%d", config->pin_num);
-
-	config->user_data = user_data;
-	user_data->fd = open(user_data->path, O_RDONLY);
-	if (user_data->fd < 0) {
-		free(user_data);
+	/* Try to open the device to check for proper pin number */
+	fd = open(path, O_RDONLY);
+	if (fd < 0) {
+		free(path);
 		return E_BAD_ARGS;
 	}
+
+	close(fd);
+	config->user_data = (void *)path;
 
 	return S_OK;
 }
 
 artik_error os_adc_release(artik_adc_config *config)
 {
-	artik_adc_user_data_t *user_data = NULL;
+	char *path = NULL;
 
 	if (!config)
 		return E_BAD_ARGS;
 
-	user_data = (artik_adc_user_data_t *)config->user_data;
-
-	if (user_data) {
-		if (user_data->path)
-			free(user_data->path);
-		free(user_data);
-	}
+	path = (char *)config->user_data;
+	if (path)
+		free(path);
 
 	return S_OK;
 }
 
 artik_error os_adc_get_value(artik_adc_config *config, int *value)
 {
-	artik_adc_user_data_t *user_data = NULL;
+	void *path = NULL;
 	struct adc_msg_s sample[S5J_ADC_MAX_CHANNELS];
 	size_t readsize;
 	ssize_t nbytes;
 	int nsamples;
+	int fd = 0;
 
 	if (!config || !value)
 		return E_BAD_ARGS;
 
-	user_data = (artik_adc_user_data_t *)config->user_data;
+	path = (char *)config->user_data;
 
-	if (ioctl(user_data->fd, ANIOC_TRIGGER, 0) < -1) {
-		close(user_data->fd);
+	fd = open(path, O_RDONLY);
+	if (fd < 0)
+		return E_BUSY;
+
+	if (ioctl(fd, ANIOC_TRIGGER, 0) < -1) {
+		close(fd);
 		return E_BUSY;
 	}
 
 	readsize = S5J_ADC_MAX_CHANNELS * sizeof(struct adc_msg_s);
-	nbytes = read(user_data->fd, sample, readsize);
+	nbytes = read(fd, sample, readsize);
 
 	if (nbytes < 0) {
-		close(user_data->fd);
+		close(fd);
 		return E_BUSY;
 	}
 
@@ -117,7 +111,7 @@ artik_error os_adc_get_value(artik_adc_config *config, int *value)
 	if (nsamples * sizeof(struct adc_msg_s) == nbytes)
 		*value = sample[config->pin_num].am_data;
 
-	close(user_data->fd);
+	close(fd);
 
 	return S_OK;
 }
