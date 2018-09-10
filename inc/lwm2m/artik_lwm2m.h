1eb5cac795ca92f218e8bbd93bcb08fcc1099204
Gregory Lemercier <g.lemercier@samsung.com>
2018-09-10 15:33:10 +0200

    lwm2m: fix typo in doxygen comment
    Change-Id: I4b24edd472ea9b3bef892f88d34e9e1b69edd07f
    Signed-off-by: Gregory Lemercier <g.lemercier@samsung.com>

--- inc/lwm2m/artik_lwm2m.h
+++ inc/lwm2m/artik_lwm2m.h
@@ -192,7 +192,7 @@ typedef struct {
 	/*!
 	 * \brief Secret key for PSK based DTLS/TLS connection
 	 *
-	 * This parameter is needed in Certificate mode, beacause in TCP
+	 * This parameter is needed in Certificate mode, because in TCP
 	 * the PSK is passed in query parameters.
 	 */
 	char *tls_psk_key;
