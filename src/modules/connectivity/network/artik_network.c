03ee42f967399f60fc755bca62e8c74d826bc446
Gregory Lemercier <g.lemercier@samsung.com>
2018-10-10 13:13:10 +0200

    network: use new external service for getting public IP
    The geoip service we used to call to get the public IP address
    of the device changed policy on Oct 8th 2018, and now require
    an API key to work. This patch replaces this service by another
    service that does not require any authentication.
    
    Change-Id: Ib15ad8c4b295111a8c3d03894c9fa1a9cb5d89b1
    Signed-off-by: Gregory Lemercier <g.lemercier@samsung.com>

--- src/modules/connectivity/network/artik_network.c
+++ src/modules/connectivity/network/artik_network.c
@@ -30,28 +30,6 @@
 #include "common_network.h"
 #include "os_network.h"
 
-static const char geoipdb_root_ca[] =
-	"-----BEGIN CERTIFICATE-----\n"
-	"MIIDSjCCAjKgAwIBAgIQRK+wgNajJ7qJMDmGLvhAazANBgkqhkiG9w0BAQUFADA/\n"
-	"MSQwIgYDVQQKExtEaWdpdGFsIFNpZ25hdHVyZSBUcnVzdCBDby4xFzAVBgNVBAMT\n"
-	"DkRTVCBSb290IENBIFgzMB4XDTAwMDkzMDIxMTIxOVoXDTIxMDkzMDE0MDExNVow\n"
-	"PzEkMCIGA1UEChMbRGlnaXRhbCBTaWduYXR1cmUgVHJ1c3QgQ28uMRcwFQYDVQQD\n"
-	"Ew5EU1QgUm9vdCBDQSBYMzCCASIwDQYJKoZIhvcNAQEBBQADggEPADCCAQoCggEB\n"
-	"AN+v6ZdQCINXtMxiZfaQguzH0yxrMMpb7NnDfcdAwRgUi+DoM3ZJKuM/IUmTrE4O\n"
-	"rz5Iy2Xu/NMhD2XSKtkyj4zl93ewEnu1lcCJo6m67XMuegwGMoOifooUMM0RoOEq\n"
-	"OLl5CjH9UL2AZd+3UWODyOKIYepLYYHsUmu5ouJLGiifSKOeDNoJjj4XLh7dIN9b\n"
-	"xiqKqy69cK3FCxolkHRyxXtqqzTWMIn/5WgTe1QLyNau7Fqckh49ZLOMxt+/yUFw\n"
-	"7BZy1SbsOFU5Q9D8/RhcQPGX69Wam40dutolucbY38EVAjqr2m7xPi71XAicPNaD\n"
-	"aeQQmxkqtilX4+U9m5/wAl0CAwEAAaNCMEAwDwYDVR0TAQH/BAUwAwEB/zAOBgNV\n"
-	"HQ8BAf8EBAMCAQYwHQYDVR0OBBYEFMSnsaR7LHH62+FLkHX/xBVghYkQMA0GCSqG\n"
-	"SIb3DQEBBQUAA4IBAQCjGiybFwBcqR7uKGY3Or+Dxz9LwwmglSBd49lZRNI+DT69\n"
-	"ikugdB/OEIKcdBodfpga3csTS7MgROSR6cz8faXbauX+5v3gTt23ADq1cEmv8uXr\n"
-	"AvHRAosZy5Q6XkjEGB5YGV8eAlrwDPGxrancWYaLbumR9YbK+rlmM6pZW87ipxZz\n"
-	"R8srzJmwN0jP41ZL9c8PDHIyh8bwRLtTcm1D9SZImlJnt1ir/md2cXjbDaJWFBM5\n"
-	"JDGFoqgCWjBH4d1QB7wCCZAA62RjYJsWvIjJEubSfZGL+T0yjWW06XyxV3bqxbYo\n"
-	"Ob8VZRzI9neWagqNdwvYkQsEjgfbKbYK7p2CNTUQ\n"
-	"-----END CERTIFICATE-----\n";
-
 static artik_error artik_set_network_config(
 		artik_network_config * config,
 		artik_network_interface_t interface);
@@ -104,21 +82,15 @@ artik_error artik_get_current_public_ip(artik_network_ip *ip)
 	char *token = NULL;
 	char delimiter[] = "\"";
 	unsigned int size = 0;
-	artik_ssl_config ssl;
 
 	log_dbg("");
 
-	memset(&ssl, 0, sizeof(artik_ssl_config));
-	ssl.verify_cert = ARTIK_SSL_VERIFY_REQUIRED;
-	ssl.ca_cert.data = (char *)geoipdb_root_ca;
-	ssl.ca_cert.len = sizeof(geoipdb_root_ca);
-
 	/* Perform the request */
-	ret = http->get("http://geoip.nekudo.com/api", NULL, &response, NULL, &ssl);
+	ret = http->get("http://ip-api.com/json", NULL, &response, NULL, NULL);
 	if (ret != S_OK)
 		goto exit;
 
-	point = strstr(response, "\"ip\":");
+	point = strstr(response, "\"query\":");
 	if (point != NULL) {
 		token = strtok(point, delimiter);
 		for (i = 0; token != NULL && i < 2; i++) {
